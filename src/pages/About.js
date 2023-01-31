c85bfdfe6e31821b88d441e1d7ff453337545cec
Ben Binder <ben.binder11@gmail.com>
2023-01-31 15:58:46 -0600

    Modify styling on nav.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
 export default function About () {
   return (
-    <div>
-      This is the about page.
-    </div>
+    <>
+    <p>
+      I am a passionate and versatile developer, designer, and educator, seeking to apply extensive e-learning and web development expertise to a new career as a full-stack engineer.
+    </p>
+    <p>
+      Visit the portfolio to check out samples of my work, or contact me to learn more about my ongoing projects!
+    </p>
+    </>
   )
 }
