033b4d687ed969c43779a413c342c1833453d22a
Ben Binder <ben.binder11@gmail.com>
2023-01-31 22:34:44 -0600

    Implement footer.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
+import Footer from './components/Footer';
 
 function App() {
   const pages = [
@@ -22,6 +23,7 @@ function App() {
     <Body
       currentPage={currentPage}
     />
+    <Footer/>
     </section>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer () {
+  return (
+    <footer className='text-center text-highlight py-4 text-[12px]'>
+      &copy; Ben Binder. All rights reserved.
+    </footer>
+  );
+}
